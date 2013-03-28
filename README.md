0e757582a0c22efd7084d38047d4c52e4251e124
DenkSchuldt <dschuldt@espol.edu.ec>
2013-03-28 15:38:43 -0700

    Front-end added

---
+++ README.md
@@ -0,0 +1,4 @@
+ConnTest
+========
+
+Minimal Websocket connection test using Node.js and HTML5
---
+++ index.js
@@ -0,0 +1,43 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+io.sockets.on('connection', function(socket){
+  socket.emit('news', {Websocket: "connection established"});
+  socket.on('my other event', function(data){
+    console.log(data);
+  });
+});
+
