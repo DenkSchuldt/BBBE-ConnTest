7257447789eb2da3812fe7d3e3c8435ad9e91863
DenkSchuldt <dschuldt@espol.edu.ec>
2013-04-05 15:41:39 -0700

    Sending multiple messages using WSS and WS.

--- index.js
+++ index.js
@@ -46,15 +46,18 @@ var httpIo = require('socket.io').listen(httpServer);
 httpIo.sockets.on('connection', function(socket){
   socket.on('connect', function(){
     console.log('   ---> WS Connected (server message)');  
+    socket.emit('established',"{Websocket: connection establisehd}");
   });
   socket.on('reconnect',function(){
     console.log('   ---> WS Reconnected (server message)');
+    socket.emit('restablished',"{Websocket: connection restablisehd}");
   });
   socket.on('send', function(message){
     httpIo.sockets.send(message);
   });
   socket.on('disconnect',function(){
     console.log('   ---> WS Disconnected (server message)');
+    socket.emit('disconnected', "{Websocket: disconnected}");
     //httpServer.close();
   });
 });
@@ -68,15 +71,18 @@ var httpsIo = require('socket.io').listen(httpsServer);
 httpsIo.sockets.on('connection', function(socket){
   socket.on('connect', function(){
     console.log('   ---> WSS Connected (server message)');
+    socket.emit('established', "{Websocket secure: connection establisehd}");
   });
   socket.on('reconnect',function(){
     console.log('   ---> WSS Reconnected (server message)');
+    socket.emit('restablished',"{Websocket secure: connection restablisehd}");
   });
   socket.on('send', function(message){
     httpsIo.sockets.send(message);
   });
   socket.on('disconnect',function(){
     console.log('   ---> WSS Disconnected (server message)');
+    socket.emit('disconnected', "{Websocket secure: disconnected}");
     //httpsServer.close();
   });
 });
